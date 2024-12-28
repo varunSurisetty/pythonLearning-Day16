c904fe9da266c5e972392627c30a153a40df8469
varunSurisetty <varunpoly143@gmail.com>
2024-12-28 14:43:11 +0530

    day 16 changes added

---
+++ main.py
@@ -0,0 +1,10 @@
+# from turtle import Turtle, Screen
+#
+# maxx = Turtle()
+# maxx.shape("turtle")
+# maxx.fillcolor("green")
+# maxx.goto(300,0)
+# sc = Screen()
+# sc.exitonclick()
+# print(maxx)
+
